ab97f53127e5bae07054b034e19859dbdb408140
Malav Naik <malavnaik12@gmail.com>
2023-03-30 22:06:06 -0400

    Fully functioning model

--- model_GA.py
+++ model_GA.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import random
 
 class ModelGA:
@@ -14,97 +13,97 @@ class ModelGA:
             {'team':'Red Bull','name': 'Sergio Perez',
                 'qualifying_position': np.nanmean([2,1]),
                 'race_position': np.nanmean([2,1]),
-                'curr_week_fp_pos': np.nanmean([2,2,2]),
+                'curr_week_fp_pos': np.nanmean([2,3,2]),
                 'price': np.nanmean(18.1)},
             {'team':'Aston Martin','name': 'Fernando Alonso',
                 'qualifying_position': np.nanmean([5,2]),
                 'race_position': np.nanmean([3,3]),
-                'curr_week_fp_pos': np.nanmean([3,3,3]),
+                'curr_week_fp_pos': np.nanmean([3,2,3]),
                 'price': np.nanmean(8.5)},
             {'team':'Aston Martin','name': 'Lance Stroll',
                 'qualifying_position': np.nanmean([8,5]),
                 'race_position': np.nanmean([6,20]),
-                'curr_week_fp_pos': np.nanmean([4,4,4]),
+                'curr_week_fp_pos': np.nanmean([4,7,4]),
                 'price': np.nanmean(7.6)},
             {'team':'Mercedes','name': 'George Russell',
                 'qualifying_position': np.nanmean([6,3]),
                 'race_position': np.nanmean([7,4]),
-                'curr_week_fp_pos': np.nanmean([5,5,5]),
+                'curr_week_fp_pos': np.nanmean([5,5,11]),
                 'price': np.nanmean(18.6)},
             {'team':'Mercedes','name': 'Lewis Hamilton',
                 'qualifying_position': np.nanmean([7,7]),
                 'race_position': np.nanmean([5,5]),
-                'curr_week_fp_pos': np.nanmean([6,6,6]),
+                'curr_week_fp_pos': np.nanmean([6,11,5]),
                 'price': np.nanmean(23.7)},
             {'team':'Ferrari','name': 'Charles Leclerc',
                 'qualifying_position': np.nanmean([3,12]),
                 'race_position': np.nanmean([19,7]),
-                'curr_week_fp_pos': np.nanmean([7,7,7]),
+                'curr_week_fp_pos': np.nanmean([11,9,6]),
                 'price': np.nanmean(21.1)},
             {'team':'Ferrari','name': 'Carlos Sainz',
                 'qualifying_position': np.nanmean([4,4]),
                 'race_position': np.nanmean([4,6]),
-                'curr_week_fp_pos': np.nanmean([8,8,8]),
+                'curr_week_fp_pos': np.nanmean([7,10,10]),
                 'price': np.nanmean(17.2)},
             {'team':'Alpine','name': 'Pierre Gasly',
                 'qualifying_position': np.nanmean([20,9]),
                 'race_position': np.nanmean([9,9]),
-                'curr_week_fp_pos': np.nanmean([9,9,9]),
+                'curr_week_fp_pos': np.nanmean([8,6,9]),
                 'price': np.nanmean(8.2)},
             {'team':'Alpine','name': 'Esteban Ocon',
                 'qualifying_position': np.nanmean([9,6]),
                 'race_position': np.nanmean([18,8]),
-                'curr_week_fp_pos': np.nanmean([10,10,10]),
+                'curr_week_fp_pos': np.nanmean([12,4,14]),
                 'price': np.nanmean(9.3)},
             {'team':'Haas','name': 'Nico Hulkenburg',
                 'qualifying_position': np.nanmean([10,10]),
                 'race_position': np.nanmean([15,12]),
-                'curr_week_fp_pos': np.nanmean([11,11,11]),
+                'curr_week_fp_pos': np.nanmean([15,27,13]),
                 'price': np.nanmean(4.2)},
             {'team':'Haas','name': 'Kevin Magnessun',
                 'qualifying_position': np.nanmean([17,13]),
                 'race_position': np.nanmean([13,10]),
-                'curr_week_fp_pos': np.nanmean([12,12,12]),
+                'curr_week_fp_pos': np.nanmean([16,15,17]),
                 'price': np.nanmean(6.7)},
             {'team':'Alfa Romeo','name': 'Valteri Bottas',
                 'qualifying_position': np.nanmean([12,14]),
                 'race_position': np.nanmean([8,19]),
-                'curr_week_fp_pos': np.nanmean([13,13,13]),
+                'curr_week_fp_pos': np.nanmean([18,20,18]),
                 'price': np.nanmean(7.8)},
             {'team':'Alfa Romeo','name': 'Guanyu Zhou',
                 'qualifying_position': np.nanmean([13,11]),
                 'race_position': np.nanmean([16,13]),
-                'curr_week_fp_pos': np.nanmean([14,14,14]),
+                'curr_week_fp_pos': np.nanmean([19,16,12]),
                 'price': np.nanmean(4.9)},
             {'team':'Mclaren','name': 'Lando Norris',
                 'qualifying_position': np.nanmean([11,19]),
                 'race_position': np.nanmean([17,17]),
-                'curr_week_fp_pos': np.nanmean([15,15,15]),
+                'curr_week_fp_pos': np.nanmean([20,12,7]),
                 'price': np.nanmean(11.1)},
             {'team':'Mclaren','name': 'Oscar Pisatri',
                 'qualifying_position': np.nanmean([18,8]),
                 'race_position': np.nanmean([20,15]),
-                'curr_week_fp_pos': np.nanmean([16,16,16]),
+                'curr_week_fp_pos': np.nanmean([14,19,8]),
                 'price': np.nanmean(6.9)},
             {'team':'AlphaTauri','name': 'Nyck de Vries',
                 'qualifying_position': np.nanmean([19,18]),
                 'race_position': np.nanmean([14,14]),
-                'curr_week_fp_pos': np.nanmean([17,17,17]),
+                'curr_week_fp_pos': np.nanmean([13,17,20]),
                 'price': np.nanmean(5.0)},
             {'team':'AlphaTauri','name': 'Yuki Tsunoda',
                 'qualifying_position': np.nanmean([14,16]),
                 'race_position': np.nanmean([11,11]),
-                'curr_week_fp_pos': np.nanmean([18,18,18]),
+                'curr_week_fp_pos': np.nanmean([10,13,19]),
                 'price': np.nanmean(4.8)},
             {'team':'Williams','name': 'Alex Albon',
                 'qualifying_position': np.nanmean([15,17]),
                 'race_position': np.nanmean([10,19]),
-                'curr_week_fp_pos': np.nanmean([19,19,19]),
+                'curr_week_fp_pos': np.nanmean([9,14,15]),
                 'price': np.nanmean(5.5)},
             {'team':'Williams','name': 'Logan Sargeant',
                 'qualifying_position': np.nanmean([16,20]),
                 'race_position': np.nanmean([12,15]),
-                'curr_week_fp_pos': np.nanmean([20,20,20]),
+                'curr_week_fp_pos': np.nanmean([17,18,16]),
                 'price': np.nanmean(4.0)}
             ]
 
@@ -171,7 +170,7 @@ class ModelGA:
             team['race_position'] = int(race/2)
             team['qualifying_position'] = int(quali/2)
             team['curr_week_fp_pos'] = int(fp_pos/2)
-            print(f"Updated Teams: {team}")
+            # print(f"Updated Teams: {team}")
 
     def fitness_function(self, team):
         # Calculate average qualifying, race positions, and free practice positions for the team
@@ -201,28 +200,70 @@ class ModelGA:
         # print(fitness_score)
         # input()
         
-        return 1 / fitness_score
+        return fitness_score
 
-    def one_point_crossover(self,parent1, parent2):
+    def one_point_crossover(self):
         # choose a random index to split the parents
-        split_index = random.randint(1, len(parent1) - 1)
+        split_index = random.randint(1, len(self.parent1) - 1)
+
+        parent1 = self.parent1['drivers'] + self.parent1['constructors']
+        parent2 = self.parent2['drivers'] + self.parent2['constructors']
         
         # create child by combining the first half of parent1 and second half of parent2
-        child = parent1[:split_index] + parent2[split_index:]
-        
-        return child
 
-    def mutation(self,individual, mutation_rate):
-        for i in range(len(individual)):
-            if random.random() < mutation_rate:
-                # randomly change one attribute of the individual
-                if i < 5:
-                    attribute = 'driver'
-                else:
-                    attribute = 'constructor'
-                individual[i][attribute] = random.choice(self.drivers) if attribute == 'driver' else random.choice(self.constructors)
+        child1 = parent1[:split_index] + parent2[split_index:]
+        child2 = parent2[:split_index] + parent1[split_index:]
+
+        drivers_indx1 = [self.driver_names.index(driver) for driver in child1[0:5]]
+        constructors_indx1 = [self.constructor_names.index(constructor) for constructor in child1[5:]]
         
-        return individual
+        drivers_indx2 = [self.driver_names.index(driver) for driver in child2[0:5]]
+        constructors_indx2 = [self.constructor_names.index(constructor) for constructor in child2[5:]]
+
+        # calculate the total cost of the team
+        total_cost1 = sum([self.drivers[index_d]['price'] for index_d in drivers_indx1] +
+                        [self.constructors[index_c]['price'] for index_c in constructors_indx1])
+        
+        
+        total_cost2 = sum([self.drivers[index_d]['price'] for index_d in drivers_indx2] +
+                        [self.constructors[index_c]['price'] for index_c in constructors_indx2])
+        
+        if ((total_cost1 or total_cost2) >= self.budget):
+            self.child1 = self.parent1
+            self.child2 = self.parent2
+        else:
+            self.child1 = {'drivers':child1[0:5],'constructors':child1[5:],'total_cost':total_cost1}
+            self.child2 = {'drivers':child2[0:5],'constructors':child2[5:],'total_cost':total_cost2}
+            if ((len(set(self.child1['drivers'])) or len(set(self.child2['drivers'])) != 5)) or (len(set(self.child1['constructors'])) or len(set(self.child2['constructors'])) != 2):
+                self.child1 = self.parent1
+                self.child2 = self.parent2
+    
+    def mutation(self,child,mut_rate):
+        child_gen = False
+        while (child_gen == False):
+            if random.random() < mut_rate:
+                if random.random() < 0.8:
+                    popped = child['drivers'].pop(random.randint(0,4))
+                    while len(child['drivers']) < 5:
+                        driver = random.choice(self.driver_names)
+                        if driver not in (child['drivers'] or popped):
+                            child['drivers'].append(driver)
+                else:
+                    popped = child['constructors'].pop(random.randint(0,1))
+                    while len(child['constructors']) < 2:
+                        constructor = random.choice(self.constructor_names)
+                        if constructor not in (child['constructors'] or popped):
+                            child['constructors'].append(constructor)
+                drivers_indx1 = [self.driver_names.index(driver) for driver in child['drivers']]
+                constructors_indx1 = [self.constructor_names.index(constructor) for constructor in child['constructors']]
+                total_cost = sum([self.drivers[index_d]['price'] for index_d in drivers_indx1] +
+                            [self.constructors[index_c]['price'] for index_c in constructors_indx1])
+                if(total_cost > self.budget):
+                    child_gen = False
+                else:
+                    child_gen = True
+                    child['total_cost'] = total_cost
+        return child
 
     def initialize_population(self):
         """
@@ -284,35 +325,37 @@ class ModelGA:
         self.constructor_names = [self.constructors[ii]['name'] for ii in range(0,len(self.constructors))]
         self.num_drivers = len(self.driver_names)
         self.num_constructors = len(self.constructor_names)
-        self.budget = 100
-        self.max_generations = 100
-        self.population_size = 100
-        self.tournament_size = 5
+        self.budget = 100.7
+        self.max_generations = 10
+        self.population_size = 1000
+        self.tournament_size = 200
         self.crossover_prob = 0.8
         self.mutation_prob = 0.05
         self.initialize_population()
         self.fitnesses = [self.fitness_function(individual) for individual in self.population]
         best_individual = self.population[self.fitnesses.index(min(self.fitnesses))]
-        print(best_individual)
+        best_team_attr = {}
         for generation in range(self.max_generations):
+            print(f"Generation: {generation}")
             next_population = []
             for i in range(self.population_size):
-                parent1 = self.tournament_selection()
-                parent2 = self.tournament_selection()
-                print(parent1,parent2)
-                input()
-        #         if random.random() < crossover_prob:
-        #             child1, child2 = self.one_point_crossover(parent1, parent2)
-        #         else:
-        #             child1, child2 = parent1, parent2
-        #         child1 = self.mutation(child1, mutation_prob)
-        #         child2 = self.mutation(child2, mutation_prob)
-        #         next_population.append(child1)
-        #         next_population.append(child2)
-        #     population = next_population
-        #     fitnesses = [self.fitness_function(individual, driver_data, constructor_data, budget) for individual in population]
-        #     best_fitness = min(fitnesses)
-        #     best_individual = population[fitnesses.index(best_fitness)]
+                self.parent1 = self.tournament_selection()
+                self.parent2 = self.tournament_selection()
+                if random.random() < self.crossover_prob:
+                    self.one_point_crossover()
+                else:
+                    self.child1, self.child2 = self.parent1, self.parent2
+                child1 = self.mutation(self.child1, self.mutation_prob)
+                child2 = self.mutation(self.child2, self.mutation_prob)
+                next_population.append(child1)
+                next_population.append(child2)
+            population = next_population
+            fitnesses = [self.fitness_function(individual) for individual in population]
+            best_fitness = min(fitnesses)
+            best_individual = population[fitnesses.index(best_fitness)]
+            best_team_attr[best_fitness] = best_individual
+        # print(list(best_team_attr.keys()))
+        print(best_team_attr[min(best_team_attr.keys())])
 
 if __name__ == '__main__':
     GA = ModelGA()
