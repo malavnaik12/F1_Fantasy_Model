b0a99d1b11051a3d481cf92de6b5f28e1e3aa831
Malav Naik <malavnaik12@gmail.com>
2024-02-29 00:09:28 -0500

    Added the skeleton code for the Prices GUI tab

--- postdata_files/postdata_gui.py
+++ postdata_files/postdata_gui.py
@@ -36,36 +36,31 @@ class postdata_gui:
             self.fp_session_menu = OptionMenu(self.positions_mframe, self.session_num_var, *self.fp_sessions_nums, command=self.get_fp_session)
             self.fp_session_menu.grid(row=4,column=2,sticky='w')
         else:
-            # try:
-            #     self.fp_session_label.grid_forget()
-            #     self.fp_session_menu.grid_forget()
-            # except:
-            #     pass
-            self.other_session_label = Label(self.positions_mframe, text=f'Override Data for {self.session_type}?',font=("Helvetica 8"))
+            self.other_session_label = Label(self.positions_mframe, text=f'Override Pos. Data for {self.session_type}?',font=("Helvetica 8"))
             self.other_session_label.grid(row=5,column=0,sticky='w')
             self.other_override = IntVar()
             self.other_override.set(1)
-            self.other_session_override = Checkbutton(self.positions_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.other_override, command=self.override_true)
+            self.other_session_override = Checkbutton(self.positions_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.other_override, command=self.pos_override)
             self.other_session_override.grid(row=5,column=2,sticky='w')
         self.out_dict["session_info"]["session_type"] = self.session_types[self.session_type]
     
     def get_fp_session(self, fp_session):
         self.out_dict["session_info"]["session_num"] = fp_session
-        self.fp_session_override_label = Label(self.positions_mframe, text=f'Override Data for FP{fp_session}?',font=("Helvetica 8"))
+        self.fp_session_override_label = Label(self.positions_mframe, text=f'Override Pose. Data for FP{fp_session}?',font=("Helvetica 8"))
         self.fp_session_override_label.grid(row=5,column=0,sticky='w')
         self.fp_override = IntVar()
         self.fp_override.set(1)
-        self.fp_session_override = Checkbutton(self.positions_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.fp_override, command=self.override_true)
+        self.fp_session_override = Checkbutton(self.positions_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.fp_override, command=self.pos_override)
         self.fp_session_override.grid(row=5,column=2,sticky='w')
 
-    def override_true(self):
+    def pos_override(self):
         if self.session_type == self.sessions[0]:
             fp_override = self.fp_override.get()
             if fp_override:
-                session = True
+                override = True
             else:
-                session = False
-            self.out_dict["session_info"]["fp_override"] = session
+                override = False
+            self.out_dict["session_info"]["fp_override"] = override
         else:
             other_session_override = self.other_override.get()
             if self.session_type == self.sessions[1]:
@@ -80,48 +75,41 @@ class postdata_gui:
 
     def get_drivers(self,team):
         try:
-            self.drivers_label.grid_remove()
-            self.drivers_menu.grid_remove()
+            self.driver_label1.grid_remove()
+            self.driver_val1.grid_remove()
+            self.driver_label2.grid_remove()
+            self.driver_val2.grid_remove()
+            self.enter_driver_pos.grid_remove()
         except:
             pass
         self.curr_team = team.lower()
         drivers = [driver.capitalize() for driver in self.out_dict['current_week'][self.curr_team]]
-        self.drivers_label = Label(self.positions_mframe, text='Select Driver: ',font=("Helvetica 8"))
-        self.drivers_label.grid(row=7,column=0,sticky='w')
-        self.driver_var = StringVar(self.positions_mframe)
-        self.driver_var.set("Select Option")
-        self.drivers_menu = OptionMenu(self.positions_mframe, self.driver_var, *drivers, command=self.get_driver_info)
-        self.drivers_menu.grid(row=7,column=2,sticky='w')
-    
-    def get_driver_info(self,driver):
-        try:
-            self.drivers_label_pos.grid_remove()
-            self.driver_pos_info.grid_remove()
-            self.enter_driver_pos.grid_remove()
-        except:
-            pass
-        driver = self.driver_var.get()
-        self.curr_driver = driver.lower()
-        self.drivers_label_pos = Label(self.positions_mframe, text=f'Enter Position for {driver}: ',font=("Helvetica 8"))
-        self.drivers_label_pos.grid(row=8,column=0,sticky='w')
-        self.driver_pos_info = Entry(self.positions_mframe, width=5)
-        self.driver_pos_info.grid(row=8,column=2,sticky='w')
+        self.driver1 = drivers[0].lower()
+        self.driver2 = drivers[1].lower()
+        self.driver_label1 = Label(self.positions_mframe, text=f"Enter Position for {drivers[0]}",font=("Helvetica 8"))
+        self.driver_label1.grid(row=7,column=0,sticky='w')
+        self.driver_val1 = Entry(self.positions_mframe, width=5)
+        self.driver_val1.grid(row=7,column=2,sticky='w')
+        self.driver_label2 = Label(self.positions_mframe, text=f"Enter Position for {drivers[1]}",font=("Helvetica 8"))
+        self.driver_label2.grid(row=8,column=0,sticky='w')
+        self.driver_val2 = Entry(self.positions_mframe, width=5)
+        self.driver_val2.grid(row=8,column=2,sticky='w')
         self.enter_driver_pos = Button(self.positions_mframe,text="Enter",command=self.set_driver_info)
         self.enter_driver_pos.grid(row=8,column=2,sticky='e')
     
     def set_driver_info(self):
-        pos = self.driver_pos_info.get()
+        pos1 = self.driver_val1.get()
+        pos2 = self.driver_val2.get()
         self.count+=1
         try:
-            pos_num = int(pos)
+            pos_num1 = int(pos1)
+            pos_num2 = int(pos2)
+            assert(20 >= pos_num1 > 0 )
+            assert(20 >= pos_num2 > 0 )
         except: 
             raise ValueError("Enter a number between 1 and 20.")
-        self.out_dict['current_week'][self.curr_team][self.curr_driver] = pos_num
-        # self.drivers_label.grid_remove()
-        # self.drivers_menu.grid_remove()
-        # self.drivers_label_pos.grid_remove()
-        # self.driver_pos_info.grid_remove()
-        # self.enter_driver_pos.grid_remove()
+        self.out_dict['current_week'][self.curr_team][self.driver1] = pos_num1
+        self.out_dict['current_week'][self.curr_team][self.driver2] = pos_num2
         if self.count==1:
             self.create_pos_yaml_file = Button(self.positions_mframe,text="Create Inputs",command=self.create_positions_yaml)
             self.create_pos_yaml_file.grid(row=9,column=2,sticky='e')
@@ -129,6 +117,14 @@ class postdata_gui:
     def create_positions_yaml(self):
         with open('test.yaml',"w") as file:
             yaml.dump(self.out_dict, file, indent=2)
+
+    def price_override(self):
+        constructor_override = self.constructor_price_override.get()
+        driver_override = self.driver_price_override.get()
+    
+    def get_price_info(self,team):
+        self.price_team = team.lower()
+        print(self.price_team)
 
     def main(self):
         self.root = Tk()
@@ -146,10 +142,10 @@ class postdata_gui:
         self.notes.grid(row=0,column=0,sticky='nsew')
         self.positions_mframe = ttk.Frame(self.notes,borderwidth=2,relief='sunken')
         self.notes.add(self.positions_mframe, text='Positions')
-        self.gui_title = Label(self.positions_mframe, text=f"GA-Powered F1 Fantasy",anchor='center',font=("Helvetica 12"))
-        self.gui_title.grid(row=10,column=0,columnspan=4,sticky='nsew')
-        lbl = Label(self.positions_mframe,image=img)
-        lbl.grid(row=0,column=0,columnspan=4,sticky='nsew')
+        self.gui_title1 = Label(self.positions_mframe, text=f"GA-Powered F1 Fantasy",anchor='center',font=("Helvetica 12"))
+        self.gui_title1.grid(row=10,column=0,columnspan=4,sticky='nsew')
+        lbl_1 = Label(self.positions_mframe,image=img)
+        lbl_1.grid(row=0,column=0,columnspan=4,sticky='nsew')
         self.positions_label1 = Label(self.positions_mframe, text='Current Week Positions\nDrivers and Constructors',anchor='center',font=("Helvetica 12"))
         self.positions_label1.grid(row=1,column=0,columnspan=4,sticky='nsew')
         self.line1 = ttk.Separator(self.positions_mframe).grid(row=2,column=0,columnspan=4,sticky='nsew')
@@ -168,7 +164,6 @@ class postdata_gui:
         self.positions_mframe.grid_columnconfigure(0, weight=1)
         self.positions_mframe.grid_columnconfigure(1, weight=1)
         self.positions_mframe.grid_columnconfigure(2, weight=1)
-        self.positions_mframe.grid_columnconfigure(3, weight=1)
         self.sessions_label = Label(self.positions_mframe, text='Session Type: ',font=("Helvetica 8"))
         self.sessions_label.grid(row=3,column=0,sticky='w')
         self.session_var = StringVar(self.positions_mframe)
@@ -185,21 +180,63 @@ class postdata_gui:
         
         self.prices_mframe = ttk.Frame(self.notes,borderwidth=2,relief='sunken')
         self.notes.add(self.prices_mframe, text='Prices')
-        lbl = Label(self.prices_mframe,image=img)
-        lbl.grid(row=0,column=0,columnspan=4,sticky='nsew')
+        lbl_2 = Label(self.prices_mframe,image=img)
+        lbl_2.grid(row=0,column=0,columnspan=4,sticky='nsew')
         self.prices_label1 = Label(self.prices_mframe, text='Current Week Prices\nDrivers and Constructors',anchor='center',font=("Helvetica 12"))
         self.prices_label1.grid(row=1,column=0,columnspan=4,sticky='nsew')
-        self.line1 = ttk.Separator(self.prices_mframe).grid(row=2,column=0,columnspan=4,sticky='nsew')
-        self.line2 = ttk.Separator(self.prices_mframe).grid(row=2,column=1,rowspan=5,sticky='nsew')
+        self.line3 = ttk.Separator(self.prices_mframe).grid(row=2,column=0,columnspan=4,sticky='nsew')
+        self.line4 = ttk.Separator(self.prices_mframe).grid(row=2,column=1,rowspan=5,sticky='nsew')
+        self.prices_mframe.grid_rowconfigure(0, weight=1)
+        self.prices_mframe.grid_rowconfigure(1, weight=1)
+        self.prices_mframe.grid_rowconfigure(2, weight=1)
+        self.prices_mframe.grid_rowconfigure(3, weight=1)
+        self.prices_mframe.grid_rowconfigure(4, weight=1)
+        self.prices_mframe.grid_rowconfigure(5, weight=1)
+        self.prices_mframe.grid_rowconfigure(6, weight=1)
+        self.prices_mframe.grid_rowconfigure(7, weight=1)
+        self.prices_mframe.grid_rowconfigure(8, weight=1)
+        self.prices_mframe.grid_rowconfigure(9, weight=1)
+        self.prices_mframe.grid_rowconfigure(10, weight=1)
+        self.prices_mframe.grid_columnconfigure(0, weight=1)
+        self.prices_mframe.grid_columnconfigure(1, weight=1)
+        self.prices_mframe.grid_columnconfigure(2, weight=1)
+        self.budget = Label(self.prices_mframe, text=f"Team Budget (millions)",font=("Helvetica 8"))
+        self.budget.grid(row=3,column=0,sticky='w')
+        self.budget_val = Entry(self.prices_mframe, width=10)
+        self.budget_val.grid(row=3,column=2,sticky='w')
+        self.constructor_price_override_label = Label(self.prices_mframe, text=f'Override Constructor Price Data?',font=("Helvetica 8"))
+        self.constructor_price_override_label.grid(row=4,column=0,sticky='w')    
+        self.constructor_price_override = IntVar()
+        self.constructor_price_override.set(1)
+        self.constructor_price_override_input = Checkbutton(self.prices_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.constructor_price_override)
+        self.constructor_price_override_input.grid(row=4,column=2,sticky='w')
+        self.driver_price_override_label = Label(self.prices_mframe, text=f'Override Driver Price Data?',font=("Helvetica 8"))
+        self.driver_price_override_label.grid(row=5,column=0,sticky='w')    
+        self.driver_price_override = IntVar()
+        self.driver_price_override.set(1)
+        self.driver_price_override_input = Checkbutton(self.prices_mframe, text="Yes", onvalue=1, offvalue=0, variable=self.driver_price_override, command=self.price_override)
+        self.driver_price_override_input.grid(row=5,column=2,sticky='w')
+        self.constructors_price_label = Label(self.prices_mframe, text='Select Constructor: ',font=("Helvetica 8"))
+        self.constructors_price_label.grid(row=6,column=0,sticky='w')
+        self.constructor_price_name = StringVar(self.prices_mframe)
+        self.constructor_price_name.set("Select Option")
+        self.constructors_price_menu = OptionMenu(self.prices_mframe, self.constructor_price_name, *self.constructor_names, command=self.get_price_info)
+        self.constructors_price_menu.grid(row=6,column=2,sticky='w')
 
-        # self.positions_sframe1 = ttk.Frame(self.positions_mframe,borderwidth=2,relief='sunken')
-        # self.positions_sframe1.grid(row=2,column=0,sticky='nsew')
 
-        # self.prices_mframe = ttk.Frame(self.notes,borderwidth=2,relief='sunken')
-        # self.notes.add(self.prices_mframe, text='Prices')
+        self.gui_title2 = Label(self.prices_mframe, text=f"GA-Powered F1 Fantasy",anchor='center',font=("Helvetica 12"))
+        self.gui_title2.grid(row=10,column=0,columnspan=4,sticky='nsew')
 
-        # self.inputs_mframe = ttk.Frame(self.notes,borderwidth=2,relief='sunken')
-        # self.notes.add(self.inputs_mframe, text='GA Inputs')
+        self.ga_mframe = ttk.Frame(self.notes,borderwidth=2,relief='sunken')
+        self.notes.add(self.ga_mframe, text='GA Inputs')
+        lbl_3 = Label(self.ga_mframe,image=img)
+        lbl_3.grid(row=0,column=0,columnspan=4,sticky='nsew')
+        self.ga_mframe1 = Label(self.ga_mframe, text='Current Week GA Inputs\nParameters for Optimizations',anchor='center',font=("Helvetica 12"))
+        self.ga_mframe1.grid(row=1,column=0,columnspan=4,sticky='nsew')
+        self.line5 = ttk.Separator(self.ga_mframe).grid(row=2,column=0,columnspan=4,sticky='nsew')
+        self.line6 = ttk.Separator(self.ga_mframe).grid(row=2,column=1,rowspan=5,sticky='nsew')
+        self.gui_title3 = Label(self.ga_mframe, text=f"GA-Powered F1 Fantasy",anchor='center',font=("Helvetica 12"))
+        self.gui_title3.grid(row=10,column=0,columnspan=4,sticky='nsew')
 
         self.root.mainloop()
 if __name__ == "__main__":
