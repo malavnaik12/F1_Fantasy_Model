1a3f949a92268cc7bcc5c15447bb24f274827cba
Malav Naik <malavnaik12@gmail.com>
2024-06-30 23:34:59 -0500

    addressed infinite loop issues in the new baseline setup

--- mongo_database/routes/base_routes.py
+++ mongo_database/routes/base_routes.py
@@ -1,32 +1,32 @@
 import fastapi
 from models.base_model import Item
 from base_database import get_col_Constructors
-from crud.base_crud import create_item, read_item, update_item, delete_item
+from crud import base_crud
+# create_item, read_item, update_item, delete_item
 
 router = fastapi.APIRouter()
 
 @router.post("/constructors", response_model=Item)
 def create_item(item: Item, collection=fastapi.Depends(get_col_Constructors)):
-    # col_constructors.insert_one(item.model_dump())
-    return create_item(collection,item)
+    return base_crud.create_item(collection,item)
 
 @router.get("/constructors/{name}", response_model=Item)
 def read_item(name: str, collection=fastapi.Depends(get_col_Constructors)):
-    item = read_item(collection, name)
+    item = base_crud.read_item(collection, name)
     if item is None:
         raise fastapi.HTTPException(status_code=404, detail="Read Item: Item not found")
     return item
 
 @router.put("/constructors/{name}", response_model=Item)
 def update_item(name: str, item: Item, collection=fastapi.Depends(get_col_Constructors)):
-    result = update_item(collection,name,item)
+    result = base_crud.update_item(collection,name,item)
     if result.matched_count == 0:
         raise fastapi.HTTPException(status_code=404, detail="Update Item: Item not found")
     return item
 
 @router.delete("/constructors/{name}", status_code=204)
 def delete_item(name: str, collection=fastapi.Depends(get_col_Constructors)):
-    result = delete_item(collection,name)
+    result = base_crud.delete_item(collection,name)
     if result.deleted_count == 0:
         raise fastapi.HTTPException(status_code=404, detail="Delete Item: Item not found")
     return None
