640c4b7d42a1c746c8cdd94428e0e72b5901bc3a
Malav Naik <malavnaik12@gmail.com>
2024-07-29 22:35:27 -0500

    Started created vue version of F1 Fantasy GUI

--- F1_Fantasy_UI/test-project/src/App.vue
+++ F1_Fantasy_UI/test-project/src/App.vue
@@ -1,47 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <h1>
+      F1 Fantasy App
+    </h1>
+    <h2>
+      Header 2
+    </h2>
+    <img alt="F1 logo" class="logo" src="./assets/F1_logo.png" width="400" height="300" />
 
     <div class="wrapper">
+      <title>
+        F1 Fantasy App
+      </title>
       <HelloWorld msg="You did it!" />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
