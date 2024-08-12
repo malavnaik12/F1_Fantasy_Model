24ebd8a06edba74f97bc756fccbd497d61b2a93c
Malav Naik <malavnaik12@gmail.com>
2024-08-11 21:46:12 -0500

    updated Vue UI structure

---
+++ f1_fantasy_ui/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/F1_logo.png">
+  <h1>F1 Fantasy Team Builder App</h1>
+  <MainUI> 
+    
+  </MainUI>
+</template>
+
+<script>
+import MainUI from './components/MainUI.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainUI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
