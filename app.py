333286c232f6428c0c958cba83557f1738aabd2f
Malav Naik <malavnaik12@gmail.com>
2024-08-16 23:22:33 -0500

    added a session dropdown and setup all the necessary inputs within Item class

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -7,9 +7,9 @@ app = FastAPI()
 # Enable CORS
 origins = [
     "http://localhost:8080",  # Vue.js dev server
-    "http://localhost:8000",
     "http://10.0.0.159:8000",
-    "http://10.0.0.159:8080"   # FastAPI server
+    "http://10.0.0.159:8080",   # FastAPI server
+    "http://localhost:8000",
 ]
 
 app.add_middleware(
@@ -21,19 +21,38 @@ app.add_middleware(
 )
 
 class Item(BaseModel):
-    raceName: str
+    raceLoc: str
+    session: str
+    # data_override: bool
+    # constructor: str
+    # driver1: str
+    # driver2: str
+    # driver1_pos: int 
+    # driver2_pos: int
+    # substitute_driver: bool
+    # substitute_driver_name: str
+    # substitute_driver_pos: int
 
-@app.get('/api/get_list/')
+class utils():
+    def _parse_list(file_name: str):
+        with open(f"./input_files/{file_name}","r+") as item_list:
+            return [item.split('\n')[0] for item in item_list.readlines()]
+
+@app.get('/api/gp_locs/')
 def send_gp_dropdown():
-    with open("./input_files/local_gp_list.txt","r+") as gp_list:
-        gp_list = [item.split('\n')[0] for item in gp_list.readlines()]
-    return {'entity':gp_list}
+    return {'entity':utils._parse_list("list_gp_locs.txt")}
+
+@app.get('/api/sessions/')
+def send_session_types():
+    return {'entity':utils._parse_list("list_session_types.txt")}
 
 @app.post('/api/submit/')
 def post_gp_dropdown(item: Item):
-    entity = item.raceName
-    print(f"{entity}")
-    return {"status": "success", "entity": entity}
+    inputs = {}
+    for entity in list(item):
+        inputs[entity[0]] = entity[1]
+    print(f"{inputs}")
+    return {"status": "success", "entity": inputs}
 
 if __name__ == '__main__':
     import uvicorn
--- f1_fantasy_ui/src/components/RaceLocDD.vue
+++ f1_fantasy_ui/src/components/RaceLocDD.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
         <label for="entity-dropdown">Select Race Weekend: </label>
-        <select id="entity-dropdown" v-model="selectedEntity">
-        <option v-for="entity in entities" :key="entity" :value="entity">
+        <select id="entity-dropdown" v-model="gp_loc">
+        <option v-for="entity in gp_locs" :key="entity" :value="entity">
             {{ entity }}
         </option>
         </select>
         <p></p>
-        <button @click="submitData">Submit</button>
+        <label for="entity-dropdown">Select Race Weekend Session: </label>
+        <select id="entity-dropdown" v-model="session">
+        <option v-for="entity in sessions" :key="entity" :value="entity">
+            {{ entity }}
+        </option>
+        </select>
+        <p></p>
+        <!-- <input type="text" ref="userText" v-on:keyup.enter="submitData()"> -->
+        <button @click="submitData" v-on:keyup.enter="submitData">Submit</button>
         <p>Summary: {{ returnedData }}</p>
     </div>
 </template>
@@ -18,29 +26,48 @@ export default {
     name: "RaceLocDD",
     data() {
         return {
-            entities: [], // Array to store entities from text file
-            selectedEntity: null, // The selected entity
-            returnedData: ''
+            gp_locs: [], // Array to store gp_locs from text file
+            gp_loc: null, // The selected entity
+            sessions: [],
+            session: null,
+            data_override: true,
+            constructors: [],
+            constructor: null,
+            driver1: "",
+            driver2: "",
+            driver1_pos: 0,
+            driver2_pos: 0,
+            substitute_driver: false,
+            substitute_driver_name: "",
+            substitute_driver_pos: 0,
+            returnedData: '',
         };
     },
     mounted() {
         this.getRaceLocs();
+        this.getSessions();
     },
     methods: {
         submitData() {
             this.postRaceLoc();
         },
         getRaceLocs() {
-            axios.get('http://localhost:8000/api/get_list/')
-            // axios.get('http://10.0.0.159:8000/api/get_list/')
-            .then(response => {this.entities = response.data.entity})
+            axios.get('http://localhost:8000/api/gp_locs/')
+            // axios.get('http://10.0.0.159:8000/api/gp_locs/')
+            .then(response => {this.gp_locs = response.data.entity})
+            .catch((err) => console.log(err));
+        },
+        getSessions() {
+            axios.get('http://localhost:8000/api/sessions/')
+            // axios.get('http://10.0.0.159:8000/api/sessions/')
+            .then(response => {this.sessions = response.data.entity})
             .catch((err) => console.log(err));
         },
         postRaceLoc() {
             axios.post('http://localhost:8000/api/submit',
             // axios.post('http://10.0.0.159:8000/api/submit',
-            { raceName: this.selectedEntity })
-            .then(response => {this.returnedData = response.data.entity});
+            { raceLoc: this.gp_loc , session: this.session})
+            .then(response => {this.returnedData = response.data});
         }
     }
 };
