f4bdc3dd687e97c1c8a3c7b95f881fb81d8cee9a
Malav Naik <malavnaik12@gmail.com>
2024-09-11 22:30:57 -0500

    updated base url to use render-deployed backend

--- f1_fantasy_ui/src/axios.js
+++ f1_fantasy_ui/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    // baseURL: 'https://f1-fantasy-model-backend.onrender.com',
-    baseURL: 'http://localhost:8000', // Replace with your API base URL
+    baseURL: 'https://f1-fantasy-model-backend.onrender.com',
+    // baseURL: 'http://localhost:8000', // Replace with your API base URL
     headers: {
     // 'Content-Type': 'application/json',
 // Add other default headers if necessary
