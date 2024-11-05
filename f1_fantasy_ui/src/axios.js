ec69a63848574a3d4da00edbb40c6871d856aadd
Malav Naik <malavnaik12@gmail.com>
2024-11-04 23:26:02 -0500

    updated backend path again

--- f1_fantasy_ui/src/axios.js
+++ f1_fantasy_ui/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://f1-fantasy-model-backend.onrender.com',
+    baseURL: 'https://f1-fantasy-model-backend.onrender.com/',
     // baseURL: 'http://localhost:8000', 
 });
 
